3c1f2272343a241e80ce83bf1624391e14dbb145
Adel Acebal <adelacebal@yahoo.com>
2017-08-25 15:31:38 -0400

    more operators

--- Operators/src/com/adelacebal/Main.java
+++ Operators/src/com/adelacebal/Main.java
@@ -43,11 +43,20 @@ public class Main {
         if (topScore < 100)
             System.out.println("You got the high score");
 
-        int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100)
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100))
             System.out.println("Greater than top score and less than 100");
 
+        if ((topScore > 90) || (secondTopScore <= 90))
+            System.out.println("One of these tests is true");
 
+        int newValue = 50;
+        if (newValue == 50)
+            System.out.println("This is not an error");
+
+        boolean isCar = false;
+        if (isCar)
+            System.out.println("This is not supposed to happen");
 
 /*
 ==
