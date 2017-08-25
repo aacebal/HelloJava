8005ad6912643428a5b79d642d265880556f0911
Adel Acebal <adelacebal@yahoo.com>
2017-08-25 15:20:14 -0400

    more operators

--- Operators/src/com/adelacebal/Main.java
+++ Operators/src/com/adelacebal/Main.java
@@ -35,6 +35,29 @@ public class Main {
         result /= 2;
         System.out.println("Result is now " + result);
 
-        
+        boolean isAlien = false;
+        if (isAlien == true)
+            System.out.println("It is an Alien!");
+
+        int topScore = 80;
+        if (topScore < 100)
+            System.out.println("You got the high score");
+
+        int secondTopScore = 81;
+        if (topScore > secondTopScore && topScore < 100)
+            System.out.println("Greater than top score and less than 100");
+
+
+
+/*
+==
+!=
+>
+<
+>=
+<=
+&&
+||
+*/
     }
 }
