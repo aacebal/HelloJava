26bad85f2ab6be13688398a559404dd5072f239c
Adel Acebal <adelacebal@yahoo.com>
2017-08-25 16:32:10 -0400

    java is good

--- StatementsWhiteSpaceAndIndenting/src/com/adelacebal/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/com/adelacebal/Main.java
@@ -3,6 +3,17 @@ package com.adelacebal;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int myVariable = 50;
+        myVariable ++;
+        myVariable --;
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                           "just another" +
+                           " simple test." );
+
+        int anotherVariable = 50;myVariable--;System.out.println("This is an inline test");
+
+        
     }
 }
