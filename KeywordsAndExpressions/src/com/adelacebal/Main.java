f1b60af868aac7e21f7e75de7392864daba25b68
Adel Acebal <adelacebal@yahoo.com>
2017-08-25 16:25:16 -0400

    statements white spaces and indenting

--- KeywordsAndExpressions/src/com/adelacebal/Main.java
+++ KeywordsAndExpressions/src/com/adelacebal/Main.java
@@ -26,5 +26,11 @@ public class Main {
 //                             |                     |
             System.out.println("This is an expression");
         }
+
+        int score = 100; // score = 100
+        if (score > 99) { // score > 99
+            System.out.println("You got the high score"); // "You got the high score"
+            score = 0; // score = 0
+        }
     }
 }
