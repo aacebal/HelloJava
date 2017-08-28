7202d75161aab09d3f5cdae1db37e515c099712b
Adel Acebal <adelacebal@yahoo.com>
2017-08-28 11:59:31 -0400

    even number solution

--- While/src/com/adelacebal/Main.java
+++ While/src/com/adelacebal/Main.java
@@ -3,6 +3,29 @@ package com.adelacebal;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//	    int count = 0;
+//	    while(count != 5) {
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+//
+//        for(int i = 6; i != 6; i++) {
+//            System.out.println("Count value is " + count);
+//        }
+//
+//        count = 6;
+//        do {
+//            System.out.println("Count value was " + count);
+//            count ++;
+//        } while(count <= 6);
+    }
+
+    public static boolean isEvenNumber(int n) {
+        if (n % 2 != 0) {
+            System.out.println(n + " is not an even number");
+            return false;
+        }
+        System.out.println(n + " is an even number");
+        return true;
     }
 }
