b0ee5a5109973df342adcf632b5d7d8e717165b9
Adel Acebal <adelacebal@yahoo.com>
2017-08-28 11:45:19 -0400

    for statement finished

--- ForLoop/src/com/adelacebal/Main.java
+++ ForLoop/src/com/adelacebal/Main.java
@@ -4,12 +4,39 @@ public class Main {
 
     public static void main(String[] args) {
 //        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2));
-        for (int i = 1; i <= 10 ; i++) {
-            System.out.println("$10,000 at " + i + "% interest = " + calculateInterest(10000.0, i));
+//        for (int i = 2; i <= 10 ; i++) {
+//            System.out.println("$10,000 at " + i + "% interest = " + String.format("%.0f", calculateInterest(10000.0, i)));
+//        }
+//        for (int i = 8; i >= 2 ; i--) {
+//            System.out.println("$10,000 at " + i + "% interest = " + String.format("%.0f", calculateInterest(10000.0, i)));
+//        }
+
+        int primeCounter = 0;
+        for (int i = 0; i <= 1000; i++) {
+            boolean primeNumber = isPrime(i);
+            if (primeNumber == true) {
+                System.out.println( i + " is a Prime Number");
+                primeCounter ++;
+                if (primeCounter > 20) {
+                    break;
+                }
+            }
         }
     }
 
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate/100));
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for(int i = 2; i <= n/2; i ++) {
+            if(n % i == 0 ){
+                return false;
+            }
+        }
+        return true;
     }
 }
