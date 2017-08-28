3a97df7099a1008b188420b23223dfad7ac91cc4
Adel Acebal <adelacebal@yahoo.com>
2017-08-28 09:41:39 -0400

    overloading methods

--- MethodOverloading/src/com/adelacebal/Main.java
+++ MethodOverloading/src/com/adelacebal/Main.java
@@ -3,6 +3,26 @@ package com.adelacebal;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int newScore = calculateScore("Adel", 500);
+        System.out.println("New Score is " + newScore);
+        calculateScore(75);
+        calculateScore();
     }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score");
+        return 0;
+    }
+
+    
 }
